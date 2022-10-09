25de6e148b73fdee085396f346cd8ccad3ad6e07
John Sandall <2884159+john-sandall@users.noreply.github.com>
2022-10-09 17:41:42 +0100

    Remove Emlyn's May slide files

--- 2022-10-03_PyData-News-October-2022/images/PyData London News - May 2022 slides_files/call.js
+++
@@ -1,7 +0,0 @@
-/*..Swapping the div ID with that of the Divi footer ID..*/
-(function($){
-	$(document).ready(function() {
-	var newVal = $('#footer-editor-info');
-	$('p#footer-info').html( newVal );
-});
-})(jQuery)
